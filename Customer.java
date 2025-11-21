6024c5e4e19c6d410a424e3bc014950dbf6cccdf
Ricardo Felipe Benjamin <ricardofelipebenjamin2003@gmail.com>
2025-11-21 09:48:30 -0300

    Commit 3

--- Customer.java
+++ Customer.java
@@ -54,26 +54,26 @@ public class Customer {
 
     
     // MÉTODO EXTRAÍDO NO REFACTORING 1
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
+    private double amountFor(Rental aRental) {
+    double thisAmount = 0;
 
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
+    switch (aRental.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (aRental.getDaysRented() > 2)
+                thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+            break;
 
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
+        case Movie.NEW_RELEASE:
+            thisAmount += aRental.getDaysRented() * 3;
+            break;
 
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
-        }
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (aRental.getDaysRented() > 3)
+                thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+            break;
+    }
 
         return thisAmount;
     }
