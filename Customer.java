71123bfb9979cccc0421b7400a23e85b2b66426c
Ricardo Felipe Benjamin <ricardofelipebenjamin2003@gmail.com>
2025-11-23 13:51:43 -0300

    Commit 7

--- Customer.java
+++ Customer.java
@@ -19,26 +19,42 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration<Rental> enum_rentals = rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = enum_rentals.nextElement();
 
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     each.getCharge() + "\n";
-            totalAmount += each.getCharge();
         }
 
         // add footer lines
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints +
+        result += "Amount owed is " + getTotalCharge() + "\n";
+        result += "You earned " + getTotalFrequentRenterPoints() +
                 " frequent renter points";
+
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration<Rental> enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = enum_rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration<Rental> enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = enum_rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
         return result;
     }
 }
